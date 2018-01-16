b4ce6239bf23e84c7098cc2d8c432675819418c9
dmitrykurmanov <kurmanov.work@gmail.com>
2018-01-16 17:18:33 +0300

    fixed survey model name

--- src/app/survey.component.ts
+++ src/app/survey.component.ts
@@ -1,16 +1,16 @@
-import { Component, Input } from '@angular/core';
-import * as Survey from 'survey-angular';
+import { Component, Input } from "@angular/core";
+import * as Survey from "survey-angular";
 
 @Component({
-    selector: 'survey',
-    template: `<div class="survey-container contentcontainer codecontainer"><div id="surveyElement"></div></div>`,
+  selector: "survey",
+  template: `<div class="survey-container contentcontainer codecontainer"><div id="surveyElement"></div></div>`
 })
-export class SurveyComponent  {
-    @Input() set json (value: object) {
-        const surveyModel = new Survey.ReactSurveyModel(value);
-        Survey.SurveyNG.render('surveyElement', { model: surveyModel });
-    } 
+export class SurveyComponent {
+  @Input()
+  set json(value: object) {
+    const surveyModel = new Survey.Model(value);
+    Survey.SurveyNG.render("surveyElement", { model: surveyModel });
+  }
 
-    ngOnInit() {
-    }
+  ngOnInit() {}
 }
