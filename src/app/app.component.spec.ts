5567ea1bd56235c2b98455c2b1bd8261fc6f5ed6
Alexandr Seredenkov <seredenkov.alex@gmail.com>
2019-05-28 20:47:23 +0300

    partial correction of tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed, async } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
+import { SurveyCreatorComponent } from './survey.creator.component';
+import { SurveyComponent } from './survey.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        SurveyComponent,
+        SurveyCreatorComponent
       ],
     }).compileComponents();
   }));
