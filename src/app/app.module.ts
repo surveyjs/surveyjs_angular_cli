a67ac376a5abd288251133e5b60d7baa9c28f765
dmitrykurmanov <kurmanov.work@gmail.com>
2019-08-12 13:24:21 +0300

    added survey-analytics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from "./app.component";
 import { SurveyComponent } from "./survey.component";
 import { SurveyCreatorComponent } from "./survey.creator.component";
+import { SurveyAnalyticsComponent } from "./survey.analytics.component";
 
 @NgModule({
-  declarations: [AppComponent, SurveyComponent, SurveyCreatorComponent],
+  declarations: [
+    AppComponent,
+    SurveyComponent,
+    SurveyCreatorComponent,
+    SurveyAnalyticsComponent
+  ],
   imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
