c9df4c290a093010d71a918d8d9946e36bc7758c
tsv2013 <tsv2013@users.noreply.github.com>
2020-04-11 08:36:05 +0300

    Since 1.5.19 the signaturepad is moved to the core library not a custom widget

--- src/app/survey.creator.component.ts
+++ src/app/survey.creator.component.ts
@@ -13,7 +13,7 @@ widgets.jquerybarrating(SurveyKo);
 widgets.jqueryuidatepicker(SurveyKo);
 widgets.nouislider(SurveyKo);
 widgets.select2tagbox(SurveyKo);
-widgets.signaturepad(SurveyKo);
+//widgets.signaturepad(SurveyKo);
 widgets.sortablejs(SurveyKo);
 widgets.ckeditor(SurveyKo);
 widgets.autocomplete(SurveyKo);
